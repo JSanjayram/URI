b1d01ffa80afae99f8aa96ace147653c5480da81
Sanjay Ram <j.sanjayramjs@gmail.com>
2025-03-01 15:03:19 +0530

    M

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import yt_dlp
+import os
+
+browser_profile = os.path.expanduser('~/.config/google-chrome/Default/Cookies')  # Adjust path for your OS
+
 video_url = 'https://www.youtube.com/watch?v=QqEarYb0Uaw'
 ydl_opts = {
         'format': 'bestaudio/best',
@@ -6,6 +10,8 @@ ydl_opts = {
         'disable_throttling': True,
         'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'geo_bypass': True,
+        'cookies_from_browser': browser_profile,  # Path to browser cookies
+
 
 
     }
