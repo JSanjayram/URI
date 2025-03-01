5708b7bffd17df9fe75bdc580edd24f5fc70f0a8
Sanjay Ram <j.sanjayramjs@gmail.com>
2025-03-01 14:59:33 +0530

    M

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ video_url = 'https://www.youtube.com/watch?v=QqEarYb0Uaw'
 ydl_opts = {
         'format': 'bestaudio/best',
         'nocheckcertificate': True,
+        'disable_throttling': True,
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'geo_bypass': True,
+
 
     }
 
