f490b27d448c66e25fae843e43bd69adbc736eeb
Sanjay Ram <j.sanjayramjs@gmail.com>
2025-03-01 16:05:22 +0530

    M

--- app.py
+++ app.py
@@ -1,44 +1,29 @@
+import os
 import yt_dlp
-import itertools
-import time
+from flask import Flask
 
-# List of proxy servers
-proxies = [
-    'http://89.116.34.113:80',
-    'http://219.65.73.81:80',
-    'http://103.163.244.212:82',
-    'http://14.143.130.210:80'
-]
+app = Flask(__name__)
 
-# Create an iterator to cycle through the proxies
-proxy_pool = itertools.cycle(proxies)
+@app.route('/download/<video_id>')
+def download(video_id):
+    ydl_opts = {
+          'format': 'bestaudio/best',
+          'quiet': True,
+          'proxy': 'http://your_proxy_address:port',  # Replace with your proxy details
+          'socket_timeout': 30,
+      }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            result = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
+            if 'formats' in result:
+                audio_url = result['formats'][0]['url']
+            return audio_url
+        except Exception as e:
+            return f"Error: {e}"
 
-def get_audio_url(video_id):
-    for _ in range(len(proxies)):
-        proxy = next(proxy_pool)
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'quiet': True,
-            'proxy': proxy,
-            'socket_timeout': 30,  # Increase timeout to 30 seconds
-
-        }
-
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
-                result = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
-                if 'formats' in result:
-                    audio_url = result['formats'][0]['url']
-                    return audio_url
-            except Exception as e:
-                print(f"Error with proxy {proxy}: {e}")
-                time.sleep(5)  # Wait before trying the next proxy
-
-    raise Exception("All proxies failed.")
-
-video_id = "QqEarYb0Uaw"  # Example video ID
-audio_url = get_audio_url(video_id)
-print(audio_url)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 """from flask import Flask, jsonify
